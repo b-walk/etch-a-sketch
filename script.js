859fefd7cc291e3db70e33c5db837ce4ff6b85b0
Blake <blameswalker@gmail.com>
2023-03-20 05:37:33 -0700

    Add button that clears grid; add initial side length of 100

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const INITIAL_SIDE_LENGTH = 100;
 function initializeGrid() {
     const gridContainer = document.querySelector('#grid-container');
-    for (let i = 0; i < 16; i++) {
+    gridContainer.style.cssText = `grid-template-columns: repeat(${INITIAL_SIDE_LENGTH}, 1fr);`;
+    for (let i = 0; i < INITIAL_SIDE_LENGTH ** 2; i++) {
         gridContainer.appendChild(document.createElement('div'));
     }
     const gridItems = document.querySelectorAll('#grid-container>div');
@@ -12,3 +14,13 @@ function initializeGrid() {
     }
 }
 initializeGrid();
+
+
+const clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener('click', clearGrid);
+function clearGrid() {
+    const gridItems = document.querySelectorAll('#grid-container>div');
+    for (const gridItem of gridItems) {
+        gridItem.removeAttribute('id');
+    }
+}
