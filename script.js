ff7c53959dfd03e82e844319a9e3c2c8abb3b8f5
Blake <blameswalker@gmail.com>
2023-03-20 06:03:31 -0700

    Add resize button functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const INITIAL_SIDE_LENGTH = 100;
+const INITIAL_SIDE_LENGTH = 50;
 function initializeGrid() {
     const gridContainer = document.querySelector('#grid-container');
     gridContainer.style.cssText = `grid-template-columns: repeat(${INITIAL_SIDE_LENGTH}, 1fr);`;
@@ -15,12 +15,36 @@ function initializeGrid() {
 }
 initializeGrid();
 
-
+//Enable Clear button functionality
 const clearButton = document.querySelector('#clear-button');
 clearButton.addEventListener('click', clearGrid);
 function clearGrid() {
     const gridItems = document.querySelectorAll('#grid-container>div');
     for (const gridItem of gridItems) {
         gridItem.removeAttribute('id');
+    }
+}
+
+//Enable Resize button functionality
+const resizeButton = document.querySelector('#resize-button');
+resizeButton.addEventListener('click', resize);
+function resize() {
+    const gridContainer = document.querySelector('#grid-container');
+    let newSideLength = prompt('Enter desired side length below\nNumber cannot exceed 100', '');
+    newSideLength = +newSideLength;
+    let gridItems = Array.from(document.querySelectorAll('#grid-container>div'));
+    for (const gridItem of gridItems) {
+        gridContainer.removeChild(gridItem);
+    }
+    gridContainer.style.cssText = `grid-template-columns: repeat(${newSideLength}, 1fr);`;
+    for (let i = 0; i < newSideLength ** 2; i++) {
+        gridContainer.appendChild(document.createElement('div'));
+    }
+    gridItems = document.querySelectorAll('#grid-container>div');
+    for (const gridItem of gridItems) {
+        gridItem.classList.add('inactiveGridItem');
+        gridItem.addEventListener('mouseover', () => {
+            gridItem.setAttribute('id', 'activeGridItem');
+        })
     }
 }
